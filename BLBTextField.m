4a06447522e7d2709c7f7f8fb8f72c6592ae1b48
Bryan Blackburn <blb@withay.com>
2006-04-13 00:36:18 +0000

    Update copyright text to include 2006

--- BLBTextField.m
+++ BLBTextField.m
@@ -1,5 +1,5 @@
 /*
- * Copyright Â© 2003, Bryan L Blackburn.  All rights reserved.
+ * Copyright © 2003,2006, Bryan L Blackburn.  All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions
