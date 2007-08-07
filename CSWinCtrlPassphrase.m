69bd63d8229e41da614f57ba4dc0711a988aaa3a
Bryan Blackburn <blb@withay.com>
2007-08-07 22:51:01 +0000

    Rearrange and use pragma mark for better code navigation (ticket #43)

--- CSWinCtrlPassphrase.m
+++ CSWinCtrlPassphrase.m
@@ -50,8 +50,17 @@ static const int CSWinCtrlPassphrase_ShortPassPhrase = 8;
 #define CSWINCTRLPASSPHRASE_LOC_ENTERAGAIN NSLocalizedString( @"Enter Again", @"" )
 
 
+@interface CSWinCtrlPassphrase (InternalMethods)
+- (BOOL) doPassphrasesMatch;
+- (NSMutableData *) genKeyForConfirm:(BOOL)useConfirmTab;
+@end
+
+
 @implementation CSWinCtrlPassphrase
 
+
+#pragma mark -
+#pragma mark Initialization
 - (id) init
 {
    self = [ super initWithWindowNibName:@"CSPassphrase" ];
@@ -60,157 +69,8 @@ static const int CSWinCtrlPassphrase_ShortPassPhrase = 8;
 }
 
 
-/*
- * Size the window to fit the given frame
- */
-- (void) setAndSizeWindowForView:(NSView *)theView
-{
-   NSWindow *myWindow = [ self window ];
-   NSRect contentRect = [ NSWindow contentRectForFrameRect:[ myWindow frame ]
-                                                 styleMask:[ myWindow styleMask ] ];
-   contentRect.origin.y += contentRect.size.height - [ theView frame ].size.height;
-   contentRect.size = [ theView frame ].size;
-   [ myWindow setFrame:[ NSWindow frameRectForContentRect:contentRect styleMask:[ myWindow styleMask ] ]
-               display:NO ];
-   [ myWindow setContentView:theView ];
-}
-
-
-/*
- * Return whether or not the passphrases match
- */
-- (BOOL) doPassphrasesMatch
-{
-   // XXX This may leave stuff around, but there's no way around it
-   return [ [ passphrasePhrase2 stringValue ] isEqualToString:[ passphrasePhraseConfirm stringValue ] ];
-}
-
-
-/*
- * Generate the key from the passphrase in the window; this does not verify
- * passphrases match on the confirm tab
- */
-- (NSMutableData *) genKeyForConfirm:(BOOL)useConfirmTab
-{
-   NSString *passphrase;
-   if( useConfirmTab )
-   {
-      passphrase = [ passphrasePhrase2 stringValue ];
-      // XXX Might setStringValue: leave any cruft around?
-      [ passphrasePhrase2 setStringValue:@"" ];
-      // XXX Again, anything left behind from setStringValue:?
-      [ passphrasePhraseConfirm setStringValue:@"" ];
-   }
-   else
-   {
-      passphrase = [ passphrasePhrase1 stringValue ];
-      // XXX And again, setStringValue:?
-      [ passphrasePhrase1 setStringValue:@"" ];
-   }
-
-   NSData *passphraseData = [ passphrase dataUsingEncoding:NSUnicodeStringEncoding ];
-   const unsigned char *dataBytes = [ passphraseData bytes ];
-   /*
-    * When CiphSafe was originally written, and PowerPC was the only architecture, this innocent-looking
-    * use of dataUsingEncoding: above was safe.  Now, however, with Intel-based Macs, this returns a
-    * little endian representation, and will cause it to fail with older documents.  We need to detect
-    * this and reverse bytes to put it into big endian.
-    */
-   if( dataBytes[ 0 ] == 0xFF && dataBytes[ 1 ] == 0xFE )
-   {
-      NSMutableData *newData = [ NSMutableData dataWithLength:[ passphraseData length ] ];
-      unsigned char *newBytes = [ newData mutableBytes ];
-      unsigned int position;
-      for( position = 0; position < [ passphraseData length ]; position += 2 )
-      {
-         newBytes[ position ] = dataBytes[ position + 1 ];
-         newBytes[ position + 1 ] = dataBytes[ position ];
-      }
-      [ passphraseData clearOutData ];
-      passphraseData = newData;
-   }
-   int pdLen = [ passphraseData length ];
-   NSData *dataFirst = [ passphraseData subdataWithRange:NSMakeRange( 0, pdLen / 2 ) ];
-   NSData *dataSecond = [ passphraseData subdataWithRange:NSMakeRange( pdLen / 2, pdLen - pdLen / 2 ) ];
-   [ passphraseData clearOutData ];
-   /*
-    * XXX At this point, passphrase should be cleared, however, there is no way,
-    * that I've yet found, to do that...here's hoping it gets released and the
-    * memory reused soon...
-    */
-   passphrase = nil;
-   
-   NSMutableData *keyData = [ dataFirst SHA1Hash ];
-   [ dataFirst clearOutData ];
-   NSMutableData *tmpData = [ dataSecond SHA1Hash ];
-   [ dataSecond clearOutData ];
-   [ keyData appendData:tmpData ];
-   [ tmpData clearOutData ];
-   
-   return keyData;
-}
-
-
-/*
- * Get an encryption key, making the window application-modal;
- * noteType is one of the CSPassphraseNote_* variables
- */
-- (NSMutableData *) getEncryptionKeyWithNote:(NSString *)noteType
-                            forDocumentNamed:(NSString *)docName
-{
-   [ [ self window ] setTitle:[ NSString stringWithFormat:NSLocalizedString( @"Enter passphrase for %@", @"" ),
-                                                          docName ] ];
-   [ passphraseNote1 setStringValue:NSLocalizedString( noteType, nil ) ];
-   [ self setAndSizeWindowForView:nonConfirmView ];
-   NSArray *runModeArray = [ NSArray arrayWithObjects:NSDefaultRunLoopMode, NSModalPanelRunLoopMode, nil ];
-   [ [ NSRunLoop currentRunLoop ] performSelector:@selector( makeFirstResponder: )
-                                           target:[ self window ]
-                                         argument:passphrasePhrase1
-                                            order:9999
-                                            modes:runModeArray ];
-   parentWindow = nil;
-   int windowReturn = [ NSApp runModalForWindow:[ self window ] ];
-   [ [ self window ] orderOut:self ];
-   NSMutableData *keyData = [ self genKeyForConfirm:NO ];
-   if( windowReturn == NSRunAbortedResponse )
-   {
-      [ keyData clearOutData ];
-      keyData = nil;
-   }
-
-   return keyData;
-}
-
-
-/*
- * Get an encryption key, making the window a sheet attached to the given window
- * noteType is one of the CSPassphraseNote_* variables
- */
-- (void) getEncryptionKeyWithNote:(NSString *)noteType
-                         inWindow:(NSWindow *)window
-                    modalDelegate:(id)delegate
-                   sendToSelector:(SEL)selector
-{
-   [ [ self window ] setTitle:@"" ];
-   [ passphraseNote2 setStringValue:NSLocalizedString( noteType, nil ) ];
-   [ self setAndSizeWindowForView:confirmView ];
-   NSArray *runModeArray = [ NSArray arrayWithObjects:NSDefaultRunLoopMode, NSModalPanelRunLoopMode, nil ];
-   [ [ NSRunLoop currentRunLoop ] performSelector:@selector( makeFirstResponder: )
-                                           target:[ self window ]
-                                         argument:passphrasePhrase2
-                                            order:9999
-                                            modes:runModeArray ];
-   parentWindow = window;
-   modalDelegate = delegate;
-   sheetEndSelector = selector;
-   [ NSApp beginSheet:[ self window ]
-           modalForWindow:parentWindow
-           modalDelegate:self
-           didEndSelector:nil
-           contextInfo:NULL ];
-}
-
-
+#pragma mark -
+#pragma mark Button Handling
 /*
  * Passphrase was accepted
  */
@@ -276,6 +136,8 @@ static const int CSWinCtrlPassphrase_ShortPassPhrase = 8;
 }
 
 
+#pragma mark -
+#pragma mark Sheet Handling
 /*
  * End of the "passphrases don't match" sheet
  */
@@ -312,6 +174,159 @@ static const int CSWinCtrlPassphrase_ShortPassPhrase = 8;
               modalDelegate:self
               didEndSelector:nil
               contextInfo:NULL ];
+}
+
+
+#pragma mark -
+#pragma mark Miscellaneous
+/*
+ * Size the window to fit the given frame
+ */
+- (void) setAndSizeWindowForView:(NSView *)theView
+{
+   NSWindow *myWindow = [ self window ];
+   NSRect contentRect = [ NSWindow contentRectForFrameRect:[ myWindow frame ]
+                                                 styleMask:[ myWindow styleMask ] ];
+   contentRect.origin.y += contentRect.size.height - [ theView frame ].size.height;
+   contentRect.size = [ theView frame ].size;
+   [ myWindow setFrame:[ NSWindow frameRectForContentRect:contentRect styleMask:[ myWindow styleMask ] ]
+               display:NO ];
+   [ myWindow setContentView:theView ];
+}
+
+
+/*
+ * Return whether or not the passphrases match
+ */
+- (BOOL) doPassphrasesMatch
+{
+   // XXX This may leave stuff around, but there's no way around it
+   return [ [ passphrasePhrase2 stringValue ] isEqualToString:[ passphrasePhraseConfirm stringValue ] ];
+}
+
+
+/*
+ * Generate the key from the passphrase in the window; this does not verify
+ * passphrases match on the confirm tab
+ */
+- (NSMutableData *) genKeyForConfirm:(BOOL)useConfirmTab
+{
+   NSString *passphrase;
+   if( useConfirmTab )
+   {
+      passphrase = [ passphrasePhrase2 stringValue ];
+      // XXX Might setStringValue: leave any cruft around?
+      [ passphrasePhrase2 setStringValue:@"" ];
+      // XXX Again, anything left behind from setStringValue:?
+      [ passphrasePhraseConfirm setStringValue:@"" ];
+   }
+   else
+   {
+      passphrase = [ passphrasePhrase1 stringValue ];
+      // XXX And again, setStringValue:?
+      [ passphrasePhrase1 setStringValue:@"" ];
+   }
+   
+   NSData *passphraseData = [ passphrase dataUsingEncoding:NSUnicodeStringEncoding ];
+   const unsigned char *dataBytes = [ passphraseData bytes ];
+   /*
+    * When CiphSafe was originally written, and PowerPC was the only architecture, this innocent-looking
+    * use of dataUsingEncoding: above was safe.  Now, however, with Intel-based Macs, this returns a
+    * little endian representation, and will cause it to fail with older documents.  We need to detect
+    * this and reverse bytes to put it into big endian.
+    */
+   if( dataBytes[ 0 ] == 0xFF && dataBytes[ 1 ] == 0xFE )
+   {
+      NSMutableData *newData = [ NSMutableData dataWithLength:[ passphraseData length ] ];
+      unsigned char *newBytes = [ newData mutableBytes ];
+      unsigned int position;
+      for( position = 0; position < [ passphraseData length ]; position += 2 )
+      {
+         newBytes[ position ] = dataBytes[ position + 1 ];
+         newBytes[ position + 1 ] = dataBytes[ position ];
+      }
+      [ passphraseData clearOutData ];
+      passphraseData = newData;
+   }
+   int pdLen = [ passphraseData length ];
+   NSData *dataFirst = [ passphraseData subdataWithRange:NSMakeRange( 0, pdLen / 2 ) ];
+   NSData *dataSecond = [ passphraseData subdataWithRange:NSMakeRange( pdLen / 2, pdLen - pdLen / 2 ) ];
+   [ passphraseData clearOutData ];
+   /*
+    * XXX At this point, passphrase should be cleared, however, there is no way,
+    * that I've yet found, to do that...here's hoping it gets released and the
+    * memory reused soon...
+    */
+   passphrase = nil;
+   
+   NSMutableData *keyData = [ dataFirst SHA1Hash ];
+   [ dataFirst clearOutData ];
+   NSMutableData *tmpData = [ dataSecond SHA1Hash ];
+   [ dataSecond clearOutData ];
+   [ keyData appendData:tmpData ];
+   [ tmpData clearOutData ];
+   
+   return keyData;
+}
+
+
+/*
+ * Get an encryption key, making the window application-modal;
+ * noteType is one of the CSPassphraseNote_* variables
+ */
+- (NSMutableData *) getEncryptionKeyWithNote:(NSString *)noteType
+                            forDocumentNamed:(NSString *)docName
+{
+   [ [ self window ] setTitle:[ NSString stringWithFormat:NSLocalizedString( @"Enter passphrase for %@", @"" ),
+      docName ] ];
+   [ passphraseNote1 setStringValue:NSLocalizedString( noteType, nil ) ];
+   [ self setAndSizeWindowForView:nonConfirmView ];
+   NSArray *runModeArray = [ NSArray arrayWithObjects:NSDefaultRunLoopMode, NSModalPanelRunLoopMode, nil ];
+   [ [ NSRunLoop currentRunLoop ] performSelector:@selector( makeFirstResponder: )
+                                           target:[ self window ]
+                                         argument:passphrasePhrase1
+                                            order:9999
+                                            modes:runModeArray ];
+   parentWindow = nil;
+   int windowReturn = [ NSApp runModalForWindow:[ self window ] ];
+   [ [ self window ] orderOut:self ];
+   NSMutableData *keyData = [ self genKeyForConfirm:NO ];
+   if( windowReturn == NSRunAbortedResponse )
+   {
+      [ keyData clearOutData ];
+      keyData = nil;
+   }
+   
+   return keyData;
+}
+
+
+/*
+ * Get an encryption key, making the window a sheet attached to the given window
+ * noteType is one of the CSPassphraseNote_* variables
+ */
+- (void) getEncryptionKeyWithNote:(NSString *)noteType
+                         inWindow:(NSWindow *)window
+                    modalDelegate:(id)delegate
+                   sendToSelector:(SEL)selector
+{
+   [ [ self window ] setTitle:@"" ];
+   [ passphraseNote2 setStringValue:NSLocalizedString( noteType, nil ) ];
+   [ self setAndSizeWindowForView:confirmView ];
+   NSArray *runModeArray = [ NSArray arrayWithObjects:NSDefaultRunLoopMode, NSModalPanelRunLoopMode, nil ];
+   [ [ NSRunLoop currentRunLoop ] performSelector:@selector( makeFirstResponder: )
+                                           target:[ self window ]
+                                         argument:passphrasePhrase2
+                                            order:9999
+                                            modes:runModeArray ];
+   parentWindow = window;
+   modalDelegate = delegate;
+   sheetEndSelector = selector;
+   [ NSApp beginSheet:[ self window ]
+       modalForWindow:parentWindow
+        modalDelegate:self
+       didEndSelector:nil
+          contextInfo:NULL ];
 }
 
 @end
